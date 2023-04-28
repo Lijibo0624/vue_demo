b671e9cd62cc805ad6957adb3d97b17fc2e82b09
ljb <804382990@qq.com>
2023-04-28 17:28:54 +0800

    vuex

--- src/components/count.vue
+++ src/components/count.vue
@@ -1,22 +1,33 @@
 <template>
   <div>
+    <select v-model.number='addValue'>
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+    </select>
     <button @click="add">+</button>
-    <!-- <div>{{ sum }}</div> -->
+    <div>{{ $store.state.sum }}</div>
+    <hr/>
+    <div>{{ sumCount }}</div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from '@/vuex'
 export default {
   name: 'vuexDemo',
   data () {
     return {
-      // sum: 0
+      addValue: ''
     }
+  },
+  computed: {
+    ...mapGetters()
   },
   methods: {
     add () {
-      // this.sum++
-      console.log(this.$store)
+      // this.$store.dispatch('add')
+      this.$store.commit('Add', this.addValue)
     }
   }
 }
--- src/vuex/index.js
+++ src/vuex/index.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import count from './moudules/count'
 Vue.use(Vuex)
+
 export default new Vuex.Store({
-  modules: {
-    count
+  state: {
+    sum: 0
+  },
+  actions: {
+    add (context, value) {
+      context.commit('Add', value)
+    }
+  },
+  mutations: {
+    Add (state, value) {
+      state.sum += value
+    }
+  },
+  getters: {
+    sumCount (state) {
+      return state.sum * 10
+    }
   }
 })
